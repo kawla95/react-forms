9e1917415d726c938638d51396e9dac6f27ea683
kawla95 <kawlamansur@hotmail.co.uk>
2022-06-29 19:06:54 +0100

    imported use-state to build the form + added CSS to the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,76 @@
+import { useState } from "react";
 import "./App.css";
 import FormInput from "./components/FormInput";
 
 const App = () => {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      label: "Username",
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "text",
+      placeholder: "Email",
+      label: "Email",
+    },
+    {
+      id: 3,
+      name: "birthday",
+      type: "text",
+      placeholder: "Birthday",
+      label: "Birthday",
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "text",
+      placeholder: "Password",
+      label: "Password",
+    },
+    {
+      id: 5,
+      name: "confirmPassword",
+      type: "text",
+      placeholder: "confirmPassword",
+      label: "confirmPassword",
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  console.log(values);
   return (
     <div className="App">
-      <form>
-        <FormInput placeholder="Username" />
-        <FormInput placeholder="Email Address" />
-        <FormInput placeholder="First Name" />
-        <FormInput placeholder="Last Name" />
+      <form onSubmit={handleSubmit}>
+        <h1> Register </h1>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <button>Submit</button>
       </form>
     </div>
   );
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,10 +1,11 @@
 import "./formInput.css";
 
 const FormInput = (props) => {
+  const { label, onChange, id, ...inputProps } = props;
   return (
     <div className="formInput">
-      <label>Username:</label>
-      <input placeholder={props.placeholder} />
+      <label>{label}</label>
+      <input {...inputProps} onChange={onChange} />
     </div>
   );
 };
